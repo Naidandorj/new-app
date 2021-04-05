a7bf2743a1c55ab6ee88e5e7a4015cfd003f7529
Naidandorj <59358178+Naidandorj@users.noreply.github.com>
2021-04-05 22:20:36 +0800

    Add files via upload
    my first commits

---
+++ app.js
@@ -0,0 +1,20 @@
+var birthDate=2005;
+var  year, yearJohn, yearMark;
+var currentYear = new Date().getFullYear();
+var age = currentYear - birthDate;
+var message = "tany nas:"+age;
+console.log(message);
+if(age>18)
+{
+	console.log("ok");
+}else
+console.log("no");
+var a=2;
+var b=5;
+c=a+b;
+function medeelel()
+{
+	console.log("ene bol function");
+	console.log(message);
+}
+medeelel();
---
+++ text.js
@@ -0,0 +1,2 @@
+var a =(Math.floor(Math.random()*6)+1);
+window.document.querySelector('#score-0').textContent=a;
